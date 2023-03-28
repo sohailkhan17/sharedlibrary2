46abf6c3bc9b14ed9b68b58801d0c8760327451c
sohailkhan17 <40828597+sohailkhan17@users.noreply.github.com>
2023-03-28 16:50:05 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -18,3 +18,8 @@ def newTestDownload(repo1)
 {
   git "https://github.com/prasadcloud/${repo1}"
 }
+def newDelivery(ip,appname)
+{
+             input message: 'Approve Delivery of Code', submitter: 'srini'
+               deploy adapters: [tomcat9(credentialsId: '91434be3-cdb5-4de6-b4c1-d2023c87be82', path: '', url: ${ip})], contextPath: ${appname}, war: '**/*.war'
+}
